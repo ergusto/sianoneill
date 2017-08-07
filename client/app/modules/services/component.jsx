ff2563d146bd46c7d8e44e0e0158e8b7187c4312
Fergus Ruston <fergus.ruston@gmail.com>
2017-08-07 02:26:55 +0200

    minor changes to wording on services page

--- client/app/modules/services/component.jsx
+++ client/app/modules/services/component.jsx
@@ -30,7 +30,7 @@ export default class Component extends React.Component {
 				<div className='pb4 mh4 mw-7 align-center fs5 lh-copy fw1'>
 					<h2 className='uppercase normal'>Freelance Services</h2>
 
-					<p>I can offer the creation of garment design and technical specifications, product development, CAD presentation, start up consultancy, trend & market research. I have proficient experience with Adobe Illustrator and Photoshop. Please contact me about this at <a href='mailto:sianoneilldesign@gmail.com'>sianoneilldesign@gmail.com</a> or use the <Link to='/contact'>contact form</Link>.</p>
+					<p>I can offer the creation of garment design and technical specifications, product development, CAD presentation, start up consultancy, trend & market research (subject to availability due to contractual agreements). I have proficient experience with Adobe Illustrator and Photoshop. Please contact me about this at <a href='mailto:sianoneilldesign@gmail.com'>sianoneilldesign@gmail.com</a> or use the <Link to='/contact'>contact form</Link>.</p>
 				</div>
 			</div>
 		)
